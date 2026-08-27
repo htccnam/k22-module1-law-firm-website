944eea985e33a115250ad25758ee763bd23ea501
htccnam <htccnam@gmail.com>
2026-08-27 23:51:27 +0700

    setup deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    // ---------- THAY ĐỔI ĐÂY ----------
+    // Thay <username> và <repo-name> bằng thông tin thực tế của bạn
+    base: "/k22-module1-law-firm-website/", // lưu ý có dấu "/" đầu và cuối
+    // ----------------------------------
+});
