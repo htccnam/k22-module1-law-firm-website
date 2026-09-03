0eff1187f06f2ba2cee8e19303dd00b533f7a522
htccnam <htccnam@gmail.com>
2026-09-03 09:38:39 +0700

    About:80%

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import HomePage from "./components/HomePage";
 // import Footer
 import Footer from "./components/Footer";
+import About from "./components/About";
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
 
             {/* Nội dung phía dưới của trang web */}
             <HomePage />
+            <About />
             <Footer />
         </div>
     );
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,40 @@
 import "./About.css";
+import "./Footer.css";
+import AboutImg from "../assets/png_about.png";
 
 function About() {
-    return <div className="About-containter"></div>;
+    return (
+        <div className="About-container">
+            <div className="About-img-container">
+                <img src={AboutImg} alt="ảnh about" className="About-img" />
+            </div>
+            <div className="about-imformation">
+                <p className="about-tag">#About Us</p>
+                <p className="about-imformation-high">
+                    Get the Guidance, Support, and Counsel
+                </p>
+                <p className="description">
+                    Every year hundreds of clients choose our firm because of
+                    our dedication to advocating for our clients before, during
+                    and after closing.
+                </p>
+                <div className="about-imformation__rate">
+                    <div>
+                        <p className="rate">1782</p>
+                        <p className="rate-type">Law Cases</p>
+                    </div>
+                    <div>
+                        <p className="rate">10+</p>
+                        <p className="rate-type">Lawyer</p>
+                    </div>
+                    <div>
+                        <p className="rate">98%</p>
+                        <p className="rate-type">Happy Clients</p>
+                    </div>
+                </div>
+                <button className="button">Learn More</button>
+            </div>
+        </div>
+    );
 }
+export default About;
